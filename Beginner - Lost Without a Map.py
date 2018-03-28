10c1878f38d49c4b2b33029bb394e752402757f5
landy <landy.erick20@gmail.com>
2018-03-28 17:10:10 -0600

    completed code files

---
+++ Beginner - Lost Without a Map.py
@@ -0,0 +1,14 @@
+Description:
+Given and array of integers (x), return the array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+My Solution:
+
+
+def maps(a):
+  return list(map(lambda x: x * 2, a))
---
+++ MakeUpperCase.py
@@ -0,0 +1,6 @@
+Description:
+Write function makeUpperCase.
+
+My Solution:
+
+def make_upper_case(s): return s.upper()
